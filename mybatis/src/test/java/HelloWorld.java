f00291c28a7030b585822dfef20f103a688ed276
lcc <lichangcai5689@163.com>
2021-03-11 15:26:21 +0800

    helloworld

--- mybatis/src/test/java/HelloWorld.java
+++ mybatis/src/test/java/HelloWorld.java
@@ -1,11 +1,15 @@
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 import org.study.entity.Emp;
+import org.study.mapper.EmpMapper;
 import org.study.utils.SqlSessionUtil;
 
 import java.io.IOException;
-
+import java.io.InputStream;
 
 
 /**
@@ -17,16 +21,31 @@ import java.io.IOException;
  */
 public class HelloWorld {
 
-    private static String resource = "mybatis-config-helloworld.xml";
+    private static String resource = "mybatis-config.xml";
 
     @Test
     public void test() throws IOException {
-        SqlSessionFactory sqlSessionFactory = SqlSessionUtil.getSqlSessionFactory(resource);
+        //1、构建SqlSessionFactory
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        //1.1、通过xml
+        InputStream inputStream =  Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = builder.build(inputStream);
+        //1.2、直接配置Configuration对象
+        //Configuration configuration = new Configuration();
+        //SqlSessionFactory sqlSessionFactory = builder.build(configuration);
+
+        //2、获取SqlSession
         SqlSession sqlSession = sqlSessionFactory.openSession();
         try {
-            Emp emp = sqlSession.selectOne("org.study.mapper.EmpMapper.getEmpById", 1);
+            //3、调用查询方法
+            //3.1、调用SqlSession封装方法
+            //Emp emp = sqlSession.selectOne("org.study.mapper.EmpMapper.getEmpById", 1);
+            //3.2、通过SqlSession获取mapper接口代理对象，调用mapper接口中的方法
+            EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+            Emp emp = mapper.getEmpById(1);
             System.out.println(emp);
         }finally {
+            //4、关闭SqlSession
             sqlSession.close();
         }
 
